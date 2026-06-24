11c61907b36311c601c103a372001121c7fb9c44
LucyTools <sophia.langrock.2108@gmail.com>
2026-06-24 22:32:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Alle Caches löschen und sich selbst deregistrieren
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+  );
+});
